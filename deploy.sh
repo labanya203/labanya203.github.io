a8c56a3f6f6d4da0a4d1c4e3ef09732baff25054
Labiba Labanya <labiba30@vt.edu>
2026-03-28 12:13:42 -0400

    2026-03-28 update deploy script test

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,30 @@
 # Pre-commit script for staticrypt and committing encrypted files
 # Usage: ./precommit.sh [optional commit message]
 
+# Spninner animation
+spinner() {
+    local i sp n
+    sp='/-\\|\'
+    n=${#sp}
+    printf ' '
+    while sleep 0.1; do
+        printf "%s\\b" "${sp:i++%n:1}"
+    done
+}
+
+
 # Set PowerShell execution policy for the process
 #pwsh -Command "Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process"
-# Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+echo "Setting PowerShell execution policy to RemoteSigned for the current user:"
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
 
 # Run staticrypt to encrypt index.html and plots directory
+printf 'Encrypting files with staticrypt... '
+spinner &  # Run the spinner function in the background
+SPIN_PID=$! # Capture the process ID of the spinner
 npx staticrypt ./src/index.html ./src/plots/ -r --directory docs --verbose
+kill "$SPIN_PID" # Kill the spinner process
+printf '\n' # Move to a new line after the spinner stops
 
 # Add all changed files (not in .gitignore) to git staging
 git add -u
